5e8982a57766e527615bcbb31cb4fd89f9368a73
yrutis <yves.rutishauser@uzh.ch>
2019-05-10 16:30:21 +0200

    callback fix

--- src/evaluator/Callback.py
+++ src/evaluator/Callback.py
@@ -14,11 +14,14 @@ class Histories(keras.callbacks.Callback):
 
     def on_epoch_end(self, epoch, logs={}):
         y_pred = self.model.predict(self.validation_data[0])
+        max_lenght = min(len(y_pred)-1, 1000)
         current_dict = {}
         current_dict["X"] = self.validation_data[0]
+        current_dict["X"] = current_dict["X"][0:max_lenght]
         current_dict["Y_hat"] = np.argmax(y_pred, axis=1)
+        current_dict["Y_hat"] = current_dict["Y_hat"][0:max_lenght]
         current_dict["Y"] = self.validation_data[1]
-        max_lenght = min(len(y_pred), 1000)
+        current_dict["Y"] = current_dict["Y"][0:max_lenght]
         top_k = tf.nn.top_k(y_pred[:max_lenght], k=5, sorted=True, name=None)
         sess = tf.Session()
         top_k = sess.run(top_k)
