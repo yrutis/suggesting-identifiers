ecf5564b9e26ece1b1fbce2c48f4742d0e248662
yrutis <yves.rutishauser@uzh.ch>
2019-05-13 11:01:42 +0200

    add one more layer of dropout

--- src/data/prepare_data_new.py
+++ src/data/prepare_data_new.py
@@ -195,4 +195,4 @@ def main(filename, window_size):
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
-    main("Android-Universal-Image-Loader", 8)
+    main("all_methods_train", 8)
