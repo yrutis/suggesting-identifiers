34dffd5434f3ea940351999df46692ef8d27fc2b
yrutis <yves.rutishauser@uzh.ch>
2019-06-15 15:58:33 +0200

    remove top 5 predictions in callback

--- src/trainer/Callbacks/Callback.py
+++ src/trainer/Callbacks/Callback.py
@@ -1,3 +1,5 @@
+import logging
+
 import keras
 import numpy as np
 import tensorflow as tf
@@ -27,8 +29,11 @@ class Histories(keras.callbacks.Callback):
         :param logs: logs
         :return: make 1000 predictions from validation data every 5 epochs
         '''
+
+        logger = logging.getLogger(__name__)
         if epoch % 5 == 0:
             max_lenght = min(len(self.validation_data[0]) - 1, 1000)
+            logger.info("for")
             y_pred = self.model.predict(self.validation_data[0][0:max_lenght])
 
             current_dict = {}
@@ -37,10 +42,10 @@ class Histories(keras.callbacks.Callback):
             current_dict["Y"] = self.validation_data[1][0:max_lenght]
 
             #get top k predictions
-            top_k = tf.nn.top_k(y_pred[:max_lenght], k=5, sorted=True, name=None)
-            sess = tf.Session()
-            top_k = sess.run(top_k)
-            current_dict["top_k"] = top_k
+            #top_k = tf.nn.top_k(y_pred[:max_lenght], k=5, sorted=True, name=None)
+            #sess = tf.Session()
+            #top_k = sess.run(top_k)
+            #current_dict["top_k"] = top_k
             self.currentPredictions.append(current_dict)
 
 
@@ -57,22 +62,23 @@ class Histories(keras.callbacks.Callback):
             first_x = current_dict['X'].tolist()
             first_y = current_dict['Y'].tolist()
             first_y_hat = current_dict['Y_hat'].tolist()
-            first_k_y_hat = current_dict['top_k'].indices.tolist()
-            self_k_y_probs = current_dict['top_k'].values.tolist()
+            #first_k_y_hat = current_dict['top_k'].indices.tolist()
+            #self_k_y_probs = current_dict['top_k'].values.tolist()
 
             first_y_hat = list(map(lambda x: [x], first_y_hat))
             # Creating texts
             first_x_reversed = list(map(sequence_to_text, first_x))
             first_y_reversed = list(map(sequence_to_text, first_y))
             first_y_hat_reversed = list(map(sequence_to_text, first_y_hat))
-            first_k_y_hat_reversed = list(map(sequence_to_text, first_k_y_hat))
+            #first_k_y_hat_reversed = list(map(sequence_to_text, first_k_y_hat))
 
             df = pd.DataFrame(
                 {"X": first_x_reversed,
                  "Y": first_y_reversed,
-                 "Y_hat": first_y_hat_reversed,
-                 "top_k": first_k_y_hat_reversed,
-                 "top_k_probs": self_k_y_probs})
+                 "Y_hat": first_y_hat_reversed
+                 #"top_k": first_k_y_hat_reversed,
+                 #"top_k_probs": self_k_y_probs
+                 })
 
             prediction_file = os.path.join(self.report_folder, 'myPred_epoch-' + str(epoch) + '.csv')
             df.to_csv(prediction_file)
